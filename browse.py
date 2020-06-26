f2e840a68566275fba2588f7799ad9750ac484e0
BuildTools <unconfigured@null.spigotmc.org>
2020-06-26 01:06:20 -0400

    got .mario levels to render

--- browse.py
+++ browse.py
@@ -1,30 +1,63 @@
 import sys, pygame
+import level
 
-pygame.init()
+class Browser:
+    grid_size = 50
+    size = width, height = 1000, 600
+    level = None
+    
+    def __init__(self):
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode(self.size)
+        self.clock = pygame.time.Clock()
 
+        self.ball = pygame.image.load("mario.png")
+        self.ballrect = self.ball.get_rect()
 
-size = width, height = 1000, 600
-speed = [1, 1]
-black = 0, 0, 0
+    # for dev purposes
+    def draw_grid(self):
+        red = pygame.Color("#ff0000")
+        for x in range(0, self.width, self.grid_size):    # draw horizontals
+            pygame.draw.line(self.screen, red, (x, 0), (x, self.height))
+        for y in range(0, self.height, self.grid_size):   # draw verticals
+            pygame.draw.line(self.screen, red, (0, y), (self.width, y))
 
-screen = pygame.display.set_mode(size)
+    def set_level(self, level):
+        self.level = level
 
-ball = pygame.image.load("mario.png")
-ballrect = ball.get_rect()
-clock = pygame.time.Clock()
+    def run(self):
+        running = True
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        while running:
+            # handle exiting
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False;
+                    break;
+    
+            speed = [1, 1]
+            self.ballrect = self.ballrect.move(speed)
+            if self.ballrect.left < 0 or self.ballrect.right > self.width:
+                speed[0] = -speed[0]
+            if self.ballrect.top < 0 or self.ballrect.bottom > self.height:
+                speed[1] = -speed[1]
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+            # draw
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(self.ball, self.ballrect)
+            self.draw_grid()
+            if self.level: self.level.draw(self.screen)
+            pygame.display.flip()
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+            self.clock.tick(120)
 
-    clock.tick(120)
+        sys.exit()
+
+if __name__ == '__main__':
+    project_dir = "/home/device/Projects/MND"
+    level = level.Level(project_dir)
+
+    game = Browser()
+    game.set_level(level)
+    game.run()
