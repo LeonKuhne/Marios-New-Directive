8898d627fe8171c4ef8d8e45bd5bc9d16bcc0833
Leon Kuehne <leonkuhne@gmail.com>
2026-08-29 01:44:38 -0400

    fixed lighting

--- lib/lights/light.h
+++ lib/lights/light.h
@@ -7,6 +7,7 @@ private:
   glm::vec4 pos;
   glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
   float intensity = 1000.0f;
+  float pad[3];
 public:
   Light(const glm::vec3& pos, float intensity) : pos(pos, 1.0f), intensity(intensity) {}
 };
